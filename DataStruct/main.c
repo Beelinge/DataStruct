ae386f6ee7ea8b3a6faced43c9255745325d5ea5
Mr Black <Beelin@Mantis-MacBook-Pro.local>
2017-02-19 16:22:33 +0800

    Initial Commit

---
+++ DataStruct/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStruct
+//
+//  Created by Beelin on 2017/2/19.
+//  Copyright © 2017年 Beelin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
